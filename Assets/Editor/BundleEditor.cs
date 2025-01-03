86ceb1b154efacfbcbd36a3a9e8be5bcc5a33e8e
zhenzhuo.wang <zhenzhuo.wang@appshahe.com>
2025-01-03 14:32:34 +0800

    feat: 剔除冗余AB包

--- Assets/Editor/BundleEditor.cs
+++ Assets/Editor/BundleEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Unity.VisualScripting;
 
 public class BundleEditor
 {
@@ -9,6 +10,8 @@ public class BundleEditor
 
     // key是ab包名，value是路径，所有文件夹ab包dic
     private static Dictionary<string, string> m_AllFileDir = new Dictionary<string, string>();
+    // key是ab名，value是所有依赖的路径
+    private static Dictionary<string, List<string>> m_AllPrefabDir = new Dictionary<string, List<string>>();
     // 用于过滤的list
     private static List<string> m_AllFileAB = new List<string>();
 
@@ -42,12 +45,40 @@ public class BundleEditor
             string path = AssetDatabase.GUIDToAssetPath(allStr[i]);
             Debug.LogError(path);
             EditorUtility.DisplayProgressBar("查找Prefab", "Prefab:" + path, i * 1.0f / length);
+            if (!ContainAllFileAB(path))
+            {
+                GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                string[] allDepends = AssetDatabase.GetDependencies(path);
+                List<string> allDependPaths = new List<string>();
+                for (int j = 0; j < allDepends.Length; j++)
+                {
+                    string tempPath = allDepends[j];
+                    Debug.Log(tempPath);
+                    if (!ContainAllFileAB(tempPath) && !tempPath.EndsWith(".cs"))
+                    {
+                        m_AllFileAB.Add(tempPath);
+                        allDependPaths.Add(tempPath);
+                    }
+                }
+                if (m_AllPrefabDir.ContainsKey(obj.name)){
+                    Debug.LogError(" 存在相同名字的Prefab！名字" + obj.name);
+                }
+                else
+                {
+                    m_AllPrefabDir.Add(obj.name, allDependPaths);
+                }
+            }
         }
         EditorUtility.ClearProgressBar();
     }
 
     private static bool ContainAllFileAB(string path)
     {
+        for (int i = 0; i < m_AllFileAB.Count; i++)
+        {
+            if (path == m_AllFileAB[i] || path.Contains(m_AllFileAB[i]))
+                return true;
+        }
         return false;
     }
 }
